3f463a5043e70dbc2af88f9cabc4f82466609296
Silverman42 <snkeze9@gmail.com>
2018-01-18 01:50:18 +0100

    Database configuration  tweek

--- config/database.php
+++ config/database.php
@@ -58,7 +58,7 @@ return [
             'database' => $database,
             'username' => $username,
             'password' => $password,
-            'unix_socket' => env('DB_SOCKET', ''),
+            //'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
